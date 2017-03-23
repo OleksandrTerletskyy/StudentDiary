a75af8c41d4f141e9df5c2d7d32edeca825cf834
Oleksandr <remixsid95@gmail.com>
2017-03-23 20:24:51 +0200

    Revert "Update MainWindow.xaml.cs"
    This reverts commit 901f72ae930f4aabfac8b94a7ed51af47361b483.

--- StudentDiary.UI/MainWindow.xaml.cs
+++ StudentDiary.UI/MainWindow.xaml.cs
@@ -485,8 +485,6 @@ namespace StudentDiary.UI
                 DayName = dayToAdd,
                 SubjectDayDict = new Dictionary<int, SubjectScheduleItem>()
             });
-            
-            //Review DP: You should sort _displayedShedule before loading.
             SubjectControl.LoadSchedule(_displayedSchedule);
             RefreshWeekStats();
         }
